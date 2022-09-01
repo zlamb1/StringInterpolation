15ceda93605198e2d8f96b4d8ae0dd9cad11a265
zlamb1 <zlamb@my.wctc.edu>
2022-08-31 21:12:59 -0500

    V1.

--- StringInterpolation/Program.cs
+++ StringInterpolation/Program.cs
@@ -6,7 +6,18 @@ namespace StringInterpolation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var date = DateTime.Now;
+            Console.WriteLine($"{date,40:MMMM d, yyyy}");
+            Console.WriteLine($"{date:yyyy.MM.d}");
+            Console.WriteLine($"{date:'Day' d 'of' MMMM, yyyy}");
+            Console.WriteLine($"{date:'Year:' yyyy, 'Month:' MM, 'Day:' dd}");
+            Console.WriteLine($"{date,10:dddd}");
+            Console.WriteLine($"{date,10:t}{date,10:dddd}");
+            Console.WriteLine($"{date:'h:'h, 'm:'m, 's:'s}");
+            Console.WriteLine($"{date:yyyy.MM.dd.h.m.s}");
+            var pi = Math.PI;
+            Console.WriteLine($"{pi.ToString("C4")}");
+            Console.WriteLine($"{pi,10:.###}");
         }
     }
 }
